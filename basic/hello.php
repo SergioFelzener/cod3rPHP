8013256879a7563c0690810896624f2231192c6c
Sergio Felzener <felzenerdj@gmail.com>
2020-12-14 21:07:13 -0300

    update

--- basic/hello.php
+++ basic/hello.php
@@ -1,1 +1,19 @@
 <div class="tile">Olá PHP</div>
+
+<?php 
+
+echo "Olá <br>";  // com quebra de linha html
+// echo "\n";  // quebra de linha terminal 
+echo "Mundo";
+
+?>
+
+<?= "Printando na tela sem echo" .  "<br>" ?>
+
+<?= "operação matematica" . "<br>"  ?>
+
+<?= 2 + 3  ?>
+
+<?php 
+
+phpinfo();
